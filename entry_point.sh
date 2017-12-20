75c1193cedadb7d4d7e5c8c54ddbd4f3c58ab21f
nochi0105 <wenhua4438@gmail.com>
2017-12-20 15:51:53 +0900

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 #set -e
  
-chmod -x ${TEMP_HOSTS}
 echo "Running Ansible"
 TEMP_FILE = /tmp/cakephp3-ansible
-bash -c "git clone https://github.com/nochi0105/cakephp3-ansible.git ${TEMP_FILE}"
-chmod +x ${TEMP_FILE} -R
-bash -c "ansible-playbook ${TEMP_FILE}/playbook.yml --inventory-file=${TEMP_FILE}/hosts --connection=local"
-rm -rf ${TEMP_FILE}
+bash -c "ansible-playbook playbook.yml --inventory-file=hosts --connection=local"
 
 exec "$@"
